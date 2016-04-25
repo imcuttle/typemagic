99b1037dbbb43b68f72b1a09109892e095516829
cong25825933 <492899414@qq.com>
2016-04-25 23:36:37 +0800

    2016/04/25

---
+++ README.md
@@ -0,0 +1,18 @@
+# typemagic (原生JS实现打字机效果)
+
+[The Article About It(Tell You Why Choose It)](http://moyuyc.github.io/2016/04/25/js实现打字机效果/)
+
+> 简单，高效，轻量，通用，自由
+
+## How To Play?
+So Easy
+```
+// You Want To Copy From Where?
+document.getElementById('src').type({
+	delay: 85, // millisecond.    default is 85 ms
+	dest: document.getElementById('dest'), // You Want To Copy To Where?    default is yourself
+	twinkle:'_' // the twinkly string.     default is '|'
+});
+```
+
+If You Like It, Please Star It!
---
+++ typemagic.js
@@ -0,0 +1,63 @@
+HTMLElement.prototype.type = function(op){
+	op = Object.extend({
+		delay:85,
+		dest:this,
+		twinkle:'|'
+	},op);
+	var chain = makeChain(this),f=false,html='';
+	console.log(chain);
+	var dest = op.dest;
+	dest.innerHTML='';
+	var time = setInterval(function(){
+		var str = chain.shift();
+		while(str.length>1){
+			html+=str;
+			dest.innerHTML=html;
+			str = chain.shift();
+			if(!chain.length){ 
+				clearInterval(time);
+				return;
+			}
+		}
+		html+=str;
+		if(!chain.length){ 
+			dest.innerHTML=html
+			clearInterval(time);
+			return;
+		}
+		dest.innerHTML=html+(f?op.twinkle:' ');
+		f=!f;
+	},op.delay);
+	function makeChain(node){
+		var nodes = node.childNodes;
+		var chain = [];
+		for(var i=0;i < nodes.length;i++){
+			var ne = nodes[i];
+			if(ne.nodeType==1){
+				if(ne.tagName=='SCRIPT'){
+					chain.push(ne.outerHTML);
+					continue;
+				}
+				var str = ne.cloneNode().outerHTML;
+				var last = str.lastIndexOf('<');
+				chain.push(str.substring(0,last));
+				chain = chain.concat(arguments.callee(ne));
+				chain.push(str.substring(last));
+			}else if(ne.nodeType==3){
+				chain = chain.concat(ne.textContent.trim().split(''));
+			}
+		}
+		return chain;
+	}
+}
+
+Object.extend = function(){
+	if(!arguments || !arguments.length) return {};
+	var rlt = arguments[0];
+	for(var i=0;i<arguments.length;i++){
+		var argu = arguments[i];
+		for(var k in argu)
+			rlt[k] = argu[k];
+	}
+	return rlt;
+}
